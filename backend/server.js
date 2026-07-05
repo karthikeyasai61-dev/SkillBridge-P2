aaae6e4d5933157ba30b1dcc636ceff9a92d9b6f
Karthik <karthikeyasai61@gmail.com>
2026-07-05 05:59:32 +0530

    Fix PEM key formatting on cloud environments

--- backend/server.js
+++ backend/server.js
@@ -57,6 +57,9 @@ try {
   }
 
   if (serviceAccount) {
+    if (serviceAccount.private_key) {
+      serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+    }
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount)
     });
